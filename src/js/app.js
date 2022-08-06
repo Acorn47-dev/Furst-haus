9a29e2dc4727d78b0460fd156bc89586725e3d95
ilyacynep <105362934+ilyacynep@users.noreply.github.com>
2022-08-06 16:54:36 +0300

    update
    last update

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+import { init } from "browser-sync";
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+var col_contact_content = document.querySelector(".col-contact__content");
