302c20b512ee0a82589032abe0e810ca3a01e791
ilyacynep <105362934+ilyacynep@users.noreply.github.com>
2022-08-09 05:37:19 +0300

    adaptive
    adaptive hero, about and more section for mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,23 @@
-import { init } from "browser-sync";
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-var col_contact_content = document.querySelector(".col-contact__content");
+let center = [53.338764, 9.875054];
+
+function init() {
+	let map = new ymaps.Map('map-test', {
+		center: center,
+		zoom: 17
+	});
+
+	map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+ymaps.ready(init);
