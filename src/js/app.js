1d0d707a31d1ff36e445af8224e2b74ec72adfb7
ilyacynep <105362934+ilyacynep@users.noreply.github.com>
2022-09-27 21:30:43 +0300

    error page
    add error 404 page

--- src/js/app.js
+++ src/js/app.js
@@ -51,3 +51,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 
 })
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
