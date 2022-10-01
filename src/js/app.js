fec31db6890aee71b62a4e4ff4199791e1a06df4
ilyacynep <105362934+ilyacynep@users.noreply.github.com>
2022-10-01 21:54:33 +0300

    fix
    fixed logo link

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,26 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+let center = [53.338764, 9.875054];
 
-// let center = [53.338764, 9.875054];
+function init() {
+  let map = new ymaps.Map('map-test', {
+    center: center,
+    zoom: 17
+  });
 
-// function init() {
-// 	let map = new ymaps.Map('map-test', {
-// 		center: center,
-// 		zoom: 17
-// 	});
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
 
-// 	map.controls.remove('geolocationControl'); // удаляем геолокацию
-//   map.controls.remove('searchControl'); // удаляем поиск
-//   map.controls.remove('trafficControl'); // удаляем контроль трафика
-//   map.controls.remove('typeSelector'); // удаляем тип
-//   map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-//   map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-//   map.controls.remove('rulerControl'); // удаляем контрол правил
-//   map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-// }
+ymaps.ready(init);
 
-// ymaps.ready(init);
 
 document.addEventListener("DOMContentLoaded", (e) => {
   var about = document.querySelector(".about");
@@ -62,7 +62,7 @@ var video_inside = document.querySelector(".video-inside");
 var video = document.querySelector(".houses-video__video");
 var video_btn = document.querySelector(".houses-video__video-pause");
 
-video_inside.addEventListener("click", (e) => {
+video_inside.addEventListener("click", (e) => {и
   if (video.paused) {
     video_btn.style.display = "none";
     video.play();
